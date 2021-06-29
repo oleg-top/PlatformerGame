bf4cf5f09e887b3fc1df94d0ffb9c3dc36070f12
Komarov Alexandr <akpartizan@yandex.ru>
2021-06-29 12:32:06 +0300

    New Folder
    This is my project

---
+++ Assets/Scripts/PortalManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+    public int doors = 1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (doors == 3)
+        {
+
+        }
+    }
+}
