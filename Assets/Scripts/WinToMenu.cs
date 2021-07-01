45a4662242f4d4a7c1cc16b49eec9b61a3235a6b
Komarov Alexandr <akpartizan@yandex.ru>
2021-07-01 10:23:24 +0300

    Project
    WinScene

---
+++ Assets/Scripts/WinToMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WinToMenu : MonoBehaviour
+{
+    public UnityEvent OnButtonPressed;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            OnButtonPressed.Invoke();
+        }
+    }
+}
